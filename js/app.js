a4adb2816a1189d9b3590dd71b054c0404ea7c89
alexdelete <alexdelete@list.ru>
2025-05-05 02:29:21 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
+// Получение случайного слова
+function getRandomWord() {
+  fetch('data/words.json')
+    .then(response => response.json())
+    .then(data => {
+      const randomWord = data.words[Math.floor(Math.random() * data.words.length)];
+      displayWord(randomWord);
+    });
+}
 
+// Поиск по тегам
+function searchByTag(tag) {
+  fetch('data/words.json')
+    .then(response => response.json())
+    .then(data => {
+      const filtered = data.words.filter(word => 
+        word.tags.includes(tag)
+      );
+      console.log(`Слова с тегом "${tag}":`, filtered);
+    });
+}
