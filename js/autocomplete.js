acd5f8be0c506639599065f924ddfe64404ea47c
alexdelete <alexdelete@list.ru>
2025-05-05 02:29:04 +0300

    Update autocomplete.js

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -1,1 +1,10 @@
-
+function searchWords(query) {
+  return fetch('data/words.json')
+    .then(response => response.json())
+    .then(data => {
+      return data.words.filter(item => 
+        item.word.toLowerCase().includes(query.toLowerCase()) || 
+        item.transcription.toLowerCase().includes(query.toLowerCase())
+      );
+    });
+}
