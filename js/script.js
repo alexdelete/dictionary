7f124bcb1c5181d38d41989f24890e884dbfd377
alexdelete <alexdelete@list.ru>
2025-05-09 00:21:48 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
+// Темный режим
 const toggleDarkMode = () => {
   const isDark = document.documentElement.classList.toggle('dark');
   localStorage.setItem('dark-mode', isDark);
 };
 
-// Установка начального состояния темного режима
-if (localStorage.getItem('dark-mode') === 'true' ||
-    window.matchMedia('(prefers-color-scheme: dark)').matches) {
+if (localStorage.getItem('dark-mode') === 'true' || window.matchMedia('(prefers-color-scheme: dark)').matches) {
   document.documentElement.classList.add('dark');
 }
 
 document.querySelector('.dark-mode-toggle').addEventListener('click', toggleDarkMode);
+
+// Открытие/закрытие окна категорий
+const categoryButton = document.getElementById('categoryButton');
+const categoryOptions = document.getElementById('categoryOptions');
+
+const toggleCategoryMenu = (event) => {
+  event.stopPropagation();
+  categoryOptions.classList.toggle('show');
+};
+
+// Закрытие меню при клике вне окна
+const closeCategoryMenu = (event) => {
+  if (!categoryOptions.contains(event.target) && !categoryButton.contains(event.target)) {
+    categoryOptions.classList.remove('show');
+  }
+};
+
+// Выбор категории
+const selectCategory = (event) => {
+  const selectedCategory = event.target.value;
+  if (selectedCategory) {
+    console.log(`Выбрана категория: ${selectedCategory}`); // Здесь вы можете добавить логику для обработки выбранной категории
+    categoryOptions.classList.remove('show');
+  }
+};
+
+categoryButton.addEventListener('click', toggleCategoryMenu);
+document.addEventListener('click', closeCategoryMenu);
+categoryOptions.addEventListener('click', selectCategory);
